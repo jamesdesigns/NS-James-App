b73cb17766569c9fb24f2fad9a830753700ac701
James Hooper <jameshooperdesigns@gmail.com>
2019-05-30 15:46:12 -0600

    Add new settings-view-model.js file

--- app/settings/settings-view-model.js
+++ app/settings/settings-view-model.js
@@ -3,6 +3,29 @@ const observableModule = require("tns-core-modules/data/observable");
 function SettingsViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        items: [
+            {
+                name: "Map",
+                description: "Description for Item 1"
+            },
+            {
+                name: "Delays",
+                description: "Description for Item 2"
+            },
+            {
+                name: "Privacy",
+                description: "Description for Item 3"
+            },
+            {
+                name: "Notifications",
+                description: "Description for Item 4"
+            },
+            {
+                name: "Feedback",
+                description: "Description for Item 5"
+            }
+        ]
+
     });
 
     return viewModel;
